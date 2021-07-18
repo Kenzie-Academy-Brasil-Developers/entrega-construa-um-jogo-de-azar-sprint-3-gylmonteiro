99edf0b78768a4af0951a71d4123d8bf5bb2f2cb
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2021-07-18 20:42:25 -0300

    inserido as funcionalidades dos botões do jogo

--- script.js
+++ script.js
@@ -1,13 +1,84 @@
-const resultado = document.getElementById("resultadoDisputa");
+const result = document.getElementById("resultadoDisputa")
+// console.log(result)
+
+const buttonPedra = document.getElementById("escolhaPedra");
+const buttonTesoura = document.getElementById("escolhaTesoura");
+// console.log(tesoura)
+const buttonPapel = document.getElementById("escolhaPapel");
+
 //Função de comparação
-const disputa = function () {
-    //Escolha da máquina
+// const disputa = function () {
+//     //Escolha da máquina
+//     let choices = ['papel', 'tesoura', 'pedra']
+//     let tamanho_array = choices.length
+//     let nAleatorio = Math.floor(Math.random() * tamanho_array);
+//     let retornoAleatorio = choices[nAleatorio]
+//     console.log (retornoAleatorio)
+//     };
+
+    
+buttonPedra.addEventListener('click',()=> {
+    // escolha da máquina
     let choices = ['papel', 'tesoura', 'pedra']
     let tamanho_array = choices.length
     let nAleatorio = Math.floor(Math.random() * tamanho_array);
-    let retornoAleatorio = choices[nAleatorio]
-    
-    
-    };
+    let retornoAleatorio = choices[nAleatorio];
+    const choiceUser = "pedra";
+    if (retornoAleatorio === choiceUser) {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Empatou";
+        result.appendChild(empate);
+    } else if (retornoAleatorio === "papel" ) {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Você perdeu";
+        result.appendChild(empate);
+    } else {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Você Ganhou";
+        result.appendChild(empate);
+    }
+});
 
-disputa();
+buttonTesoura.addEventListener('click',()=> {
+    // escolha da máquina
+    let choices = ['papel', 'tesoura', 'pedra']
+    let tamanho_array = choices.length
+    let nAleatorio = Math.floor(Math.random() * tamanho_array);
+    let retornoAleatorio = choices[nAleatorio];
+    const choiceUser = "tesoura";
+    if (retornoAleatorio === choiceUser) {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Empatou";
+        result.appendChild(empate);
+    } else if (retornoAleatorio === "papel" ) {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Você Ganhou";
+        result.appendChild(empate);
+    } else {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Você Perdeu";
+        result.appendChild(empate);
+    }
+})
+
+buttonPapel.addEventListener('click',()=> {
+    // escolha da máquina
+    let choices = ['papel', 'tesoura', 'pedra']
+    let tamanho_array = choices.length
+    let nAleatorio = Math.floor(Math.random() * tamanho_array);
+    let retornoAleatorio = choices[nAleatorio];
+    const choiceUser = "papel";
+    if (retornoAleatorio === choiceUser) {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Empatou";
+        result.appendChild(empate);
+    } else if (retornoAleatorio === "pedra" ) {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Você Ganhou";
+        result.appendChild(empate);
+    } else {
+        const empate = document.createElement("p");
+        empate.innerHTML ="Você Perdeu";
+        result.appendChild(empate);
+    }
+})
