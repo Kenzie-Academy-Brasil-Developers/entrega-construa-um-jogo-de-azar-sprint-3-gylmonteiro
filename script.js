8cbeb34f91ebe5be41970baacca2c383aa5e9a78
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2021-07-18 15:29:34 -0300

    inserido função de escolha do resultado!

---
+++ script.js
@@ -0,0 +1,13 @@
+const resultado = document.getElementById("resultadoDisputa");
+//Função de comparação
+const disputa = function () {
+    //Escolha da máquina
+    let choices = ['papel', 'tesoura', 'pedra']
+    let tamanho_array = choices.length
+    let nAleatorio = Math.floor(Math.random() * tamanho_array);
+    let retornoAleatorio = choices[nAleatorio]
+    
+    
+    };
+
+disputa();
